7a23b4fa236bb496f3a2721ef377ef19105456e7
esc <evertonsouzaesc@gmail.com>
2023-10-31 22:09:46 -0300

    tweak debug file

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,6 +1,7 @@
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+
 local opts = {
   sources = {
     null_ls.builtins.formatting.gofumpt,
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -48,7 +48,7 @@ return {
     vim.keymap.set('n', '<F7>', dap.step_into)
     vim.keymap.set('n', '<F8>', dap.step_over)
     vim.keymap.set('n', '<F9>', dap.continue)
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+    vim.keymap.set('n', '<leader>sb', dap.toggle_breakpoint)
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end)
@@ -75,7 +75,7 @@ return {
       },
     }
     -- toggle to see last session result. Without this ,you can't see session output in case of unhandled exception.
-    vim.keymap.set("n", "<F7>", dapui.toggle)
+    vim.keymap.set("n", "<F4>", dapui.toggle)
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
@@ -83,9 +83,48 @@ return {
 
     require('nvim-dap-virtual-text').setup {
       commented = true,
+      clear_on_continue = true,
     }
 
     -- Install golang specific config
-    require('dap-go').setup()
+    require('dap-go').setup {
+      -- Additional dap configurations can be added.
+      -- dap_configurations accepts a list of tables where each entry
+      -- represents a dap configuration. For more details do:
+      -- :help dap-configuration
+      dap_configurations = {
+        {
+          -- Must be "go" or it will be ignored by the plugin
+          type = "go",
+          name = "Random Name",
+          request = "launch",
+          program = "${file}",
+        },
+      },
+      -- delve configurations
+      delve = {
+        -- the path to the executable dlv which will be used for debugging.
+        -- by default, this is the "dlv" executable on your PATH.
+        path = "dlv",
+        -- time to wait for delve to initialize the debug session.
+        -- default to 20 seconds
+        initialize_timeout_sec = 20,
+        -- a string that defines the port to start delve debugger.
+        -- default to string "${port}" which instructs nvim-dap
+        -- to start the process in a random available port
+        port = "${port}",
+        -- additional args to pass to dlv
+        args = {},
+        -- the build flags that are passed to delve.
+        -- defaults to empty string, but can be used to provide flags
+        -- such as "-tags=unit" to make sure the test suite is
+        -- compiled during debugging, for example.
+        -- passing build flags using args is ineffective, as those are
+        -- ignored by delve in dap mode.
+        build_flags = "",
+  },
+}
+
+
   end,
 }
