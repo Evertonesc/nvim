ee1efff444daed976d5d60d3fde8e70cb2ede6bf
esc <evertonsouzaesc@gmail.com>
2024-01-02 11:06:29 -0300

    remove unused function

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -8,13 +8,6 @@ if vim.fn.has("termguicolors") then
 end
 
 
---vim.api.nvim_create_autocmd('BufWritePre', {
---	pattern = '*.go',
---	callback = function()
---		vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
---	end
---})
-
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		vim.cmd("silent! lua require('telescope.builtin').find_files()")
